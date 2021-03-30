e9985d3fe9d37e2d9fe6ca43c39712a05135f443
cpach96 <cpach96@gmail.com>
2021-03-30 11:51:16 -0400

    commit tree a274c8920571083994dff45610bf04bb82c53990

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 class Formatter {
-  //add static methods here
+  
+  static capitalize(string) {
+      return string.replace(/^\w/, (c) => c.toUpperCase());
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9- ']+/g, '');
+  }
+
+  static titleize(string) {
+     string.replace(/\w\S*/g, (w) => (w.replace(/^\w/, (c) => c.toUpperCase())));
+  }
+
+  static makeSmaller(string){
+    string = string.replace("a", "an", "but", "of", "and", "for", "at", "by", "from".toLowerCase());
+  }
 }
